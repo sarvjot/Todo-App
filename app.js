7bb34ed3ec9e61bd92b91b3eba285dcab59b3f81
sarvjot <sarvjotsingh422002@gmail.com>
2022-01-15 17:24:49 +0530

    Day 3: Implement Clear Button

---
+++ app.js
@@ -0,0 +1,16 @@
+var addTaskInp = document.querySelector("#add-task-input");
+var plusBtn = document.querySelector("#plus");
+var clearBtn = document.querySelector("#clear");
+
+clearBtn.addEventListener("click", function() {
+    addTaskInp.value = "";
+});
+
+var modelTodo = document.querySelector("#model");
+function createTodo(data){
+
+}
+
+// var checkBtn = document.querySelector("#check");
+// var editBtn = document.querySelector("#edit");
+// var deleteBtn = document.querySelector("#delete");
